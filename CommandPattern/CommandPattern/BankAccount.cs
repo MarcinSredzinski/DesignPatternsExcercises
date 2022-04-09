50c157532b073a207257d478832416ea55623708
Marcin <m.sredzinski94@gmail.com>
2022-04-09 13:22:32 +0200

    Solution added, BankAccount created.

---
+++ CommandPattern/CommandPattern/BankAccount.cs
@@ -0,0 +1,25 @@
+﻿namespace CommandPattern
+{
+    internal class BankAccount
+    {
+        private int balance;
+        private int overdraftLimit = -500;
+        public void Deposit(int amount)
+        {
+            balance += amount;
+            Console.WriteLine("Deposited: {0}, current balance: {1}", amount, balance);
+        }
+        public void Withdraw(int amount)
+        {
+            if (balance - amount >= overdraftLimit)
+            {
+                balance -= amount;
+                Console.WriteLine("Withdrawn: {0}, current balance: {1}", amount, balance);
+            }
+        }
+        public override string ToString()
+        {
+            return $"{nameof(balance)}: {balance}";
+        }
+    }
+}
